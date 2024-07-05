f2701be6b9712581d70a626d923e17dacb5769bf
Ruslan <4irus86@gmail.com>
2024-07-05 10:22:01 +0300

    dep

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  publicPath: '/Todo-vue',
   plugins: [
     vue(),
     vueJsx(),
